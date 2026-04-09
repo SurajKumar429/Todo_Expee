aa5a74f5516ff9ba78ac8d924706c76b712999a0
SurajKumar429 <surajkumarnegi45@gmail.com>
2026-04-09 14:06:22 +0530

    worked on add and delete task

--- Todo_Expee/src/App.jsx
+++ Todo_Expee/src/App.jsx
@@ -1,18 +1,45 @@
-import {useState, React} from 'react'
+import { useState, React } from "react";
 
 const App = () => {
-  const [task, setTask] = useState("")
-  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  // Function to add a task
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  // Fuction to delete a task
+  const deleteTask = (index) => {
+    const updated = tasks.filter((_, i) => i !== index);
+    setTasks(updated);
+  };
+
   return (
-    <div> 
+    <div>
       <h2>Todo_Expee App</h2>
 
-      <input type='text' placeholder='Enter Your Task' value={task} onChange={(e) => setTask(e.target.value)}>
-      </input>
+      <input
+        type="text"
+        placeholder="Enter Your Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      ></input>
 
-       <button>Add Task</button>
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}
+          <button onClick={ () => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
