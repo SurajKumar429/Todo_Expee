bdcd182219975dd497769c590665c89ea93e9d8f
SurajKumar429 <surajkumarnegi45@gmail.com>
2026-04-13 23:46:19 +0530

    Worked on Update a task

--- Todo_Expee/src/App.jsx
+++ Todo_Expee/src/App.jsx
@@ -3,12 +3,21 @@ import { useState, React } from "react";
 const App = () => {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
 
   // Function to add a task
   const addTask = () => {
     if (task.trim() === "") return;
 
+    if(editIndex !== null) {
+      const updated = [...tasks];
+      updated[editIndex] = task;
+      setTasks(updated);
+      setEditIndex(null);
+    } else {
+
     setTasks([...tasks, task]);
+    }
     setTask("");
   };
 
@@ -16,6 +25,12 @@ const App = () => {
   const deleteTask = (index) => {
     const updated = tasks.filter((_, i) => i !== index);
     setTasks(updated);
+  };
+
+  // Function to edit a task
+  const editTask = (index) => {
+    setTask(tasks[index]);
+    setEditIndex(index);
   };
 
   return (
@@ -29,12 +44,15 @@ const App = () => {
         onChange={(e) => setTask(e.target.value)}
       ></input>
 
-      <button onClick={addTask}>Add Task</button>
+      <button onClick={addTask}>
+        {editIndex !== null ? "Update Task" : "Add Task"}
+      </button>
 
       <ul>
         {tasks.map((t, index) => (
           <li key={index}>{t}
           <button onClick={ () => deleteTask(index)}>Delete</button>
+          <button onClick={ () => editTask(index)}>Edit</button>
           </li>
         ))}
       </ul>
